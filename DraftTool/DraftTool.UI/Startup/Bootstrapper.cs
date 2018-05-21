74336b81fd1d5d5157ed141be7d5b9edd9471aef
Linus <osnusmeforfasensatan@hotmail.com>
2018-05-21 16:06:46 +0200

    Added folder structure, prism, autofac, MainWindow, MainViewModel and ExitApplicationCommand.

---
+++ DraftTool/DraftTool.UI/Startup/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using DraftTool.UI.ViewModel;
+using Prism.Events;
+
+namespace DraftTool.UI.Startup
+{
+    class Bootstrapper
+    {
+        public IContainer Bootstrap()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<EventAggregator>().As<IEventAggregator>().SingleInstance();
+
+            builder.RegisterType<MainWindow>().AsSelf();
+            builder.RegisterType<MainViewModel>().AsSelf();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ DraftTool/DraftTool.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Commands;
+using Prism.Events;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DraftTool.UI.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ICommand ExitApplicationCommand { get; }
+
+        private IEventAggregator _eventAggregator;
+
+        public MainViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+
+            ExitApplicationCommand = new DelegateCommand(OnExitApplication);
+        }
+
+        private void OnExitApplication()
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
