03200dbd20a91621d23450e82e89f0f8c05f20d8
Linus <osnusmeforfasensatan@hotmail.com>
2018-05-22 11:47:43 +0200

    Added card model

---
+++ DraftTool/DraftTool.Models/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraftTool.Models
+{
+    public class Card
+    {
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public int MaxNumberOfUses { get; set; }
+    }
+}
