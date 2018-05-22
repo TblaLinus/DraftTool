561507068f999f00fecea1935a39e927eb918c8e
Linus <osnusmeforfasensatan@hotmail.com>
2018-05-22 15:44:30 +0200

    Added CardListView

--- DraftTool/DraftTool.UI/ViewModel/MainViewModel.cs
+++ DraftTool/DraftTool.UI/ViewModel/MainViewModel.cs
@@ -12,17 +12,22 @@ namespace DraftTool.UI.ViewModel
     {
         private IEventAggregator _eventAggregator;
         private IDraftMenuVM _draftMenuVM;
+        private ICardListVM _cardListVM;
         private IPageVM _activePage;
 
         public ICommand NewDraftCommand { get; }
+        public ICommand GoToCardListCommand { get; }
         public ICommand ExitApplicationCommand { get; }
         public List<Card> CardList { get; set; }
 
-        public MainViewModel(IEventAggregator eventAggregator, IDraftMenuVM draftMenuVM)
+        public MainViewModel(IEventAggregator eventAggregator, IDraftMenuVM draftMenuVM, ICardListVM cardListVM)
         {
             _eventAggregator = eventAggregator;
             _draftMenuVM = draftMenuVM;
+            _cardListVM = cardListVM;
+
             NewDraftCommand = new DelegateCommand(OnNewDraft);
+            GoToCardListCommand = new DelegateCommand(OnGoToCardList);
             ExitApplicationCommand = new DelegateCommand(OnExitApplication);
 
             CardList = Startup.CreateCards.Create();
@@ -31,6 +36,11 @@ namespace DraftTool.UI.ViewModel
         private void OnNewDraft()
         {
             ActivePage = (IPageVM)_draftMenuVM;
+        }
+
+        private void OnGoToCardList()
+        {
+            ActivePage = (IPageVM)_cardListVM;
         }
 
         private void OnExitApplication()
